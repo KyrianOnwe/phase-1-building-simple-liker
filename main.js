dbad3681f1effaf777a6cae11b9d70831888dcce
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-18 21:11:49 -0500

    commit tree d581f3a572dbf5b13de57ebd332676b43ac703f2

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const hartMe = document.getElementsByClassName("like-glyph")
+hartMe.addEventListener('click',  mimicServerCall)
 
 
 
